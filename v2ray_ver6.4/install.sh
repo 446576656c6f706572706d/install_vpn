9e9138a3904791be6bc87a4454585cba567b4db4
4465?76/65$6c6#f706572706d <92156696+446576656c6f706572706d@users.noreply.github.com>
2023-01-13 14:33:05 +0330

    Update install.sh

--- v2ray_ver6.4/install.sh
+++ v2ray_ver6.4/install.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-echo"
-1
-2
-3
-4
-5
-"
 
 echo "----------------------------------------------------"
 echo "V2ray ver 1.6.4 English"
 echo "Install & Upgrade"
 echo "----------------------------------------------------"
+
 bash <(curl -Ls https://raw.githubusercontent.com/hossinasaadi/x-ui/master/install.sh)
 
 echo "----------------------------------------------------"
 echo "install BBR v2ray"
-echo "
-in-1 BBR Plus / Original BBR / Magic BBR (Recommended)
-Supported Environments
-Centos 7, Debian 8/9, Ubuntu 16/18 tested, OVZ not supported
-The script is as follows.
-"
-
+echo " in-1 BBR Plus / Original BBR / Magic BBR (Recommended)"
+echo "Supported Environments"
+echo"Centos 7, Debian 8/9, Ubuntu 16/18 tested, OVZ not supported"
+echo "The script is as follows."
 echo "----------------------------------------------------"
+
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
 
