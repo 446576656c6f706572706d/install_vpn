7b62152a7d0a43dce8c81d4dbc4e4949fe7f2058
4465?76/65$6c6#f706572706d <92156696+446576656c6f706572706d@users.noreply.github.com>
2023-01-13 23:44:27 +0330

    Update ipv6_disable.sh

--- google_capctha/ipv6_disable.sh
+++ google_capctha/ipv6_disable.sh
@@ -5,12 +5,9 @@ echo "net.ipv6.conf.all.disable_ipv6=1"
 echo "net.ipv6.conf.default.disable_ipv6=1"
 echo "net.ipv6.conf.lo.disable_ipv6=1"
 
-format_time() {
-  ((h=${1}/3600))
-  ((m=(${1}%3600)/60))
-  ((s=${1}%60))
-  printf "%02d:%02d:%02d\n" $h $m $s
- }
+echo "sleep 5"
+sleep 5
+
 
 
 echo "ok"
