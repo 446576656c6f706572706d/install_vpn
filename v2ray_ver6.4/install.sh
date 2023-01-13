f37cbf6d53c5e11b7a6c75e34f15dbcb761f3eac
4465?76/65$6c6#f706572706d <92156696+446576656c6f706572706d@users.noreply.github.com>
2023-01-13 20:35:53 +0330

    Update install.sh

--- v2ray_ver6.4/install.sh
+++ v2ray_ver6.4/install.sh
@@ -13,7 +13,11 @@ echo " in-1 BBR Plus / Original BBR / Magic BBR (Recommended)"
 echo "Supported Environments"
 echo "Centos 7, Debian 8/9, Ubuntu 16/18 tested, OVZ not supported"
 echo "The script is as follows."
+echo "run 3 scripts => instan => 1 , 2 , 3 , 8" 
 echo "----------------------------------------------------"
 
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
 
