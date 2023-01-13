d7f713bce75bd0b8aaa4f5d3f52891f1111c0b8d
4465?76/65$6c6#f706572706d <92156696+446576656c6f706572706d@users.noreply.github.com>
2023-01-13 23:15:34 +0330

    Update ipv6_disable.sh

--- google_capctha/ipv6_disable.sh
+++ google_capctha/ipv6_disable.sh
@@ -5,21 +5,12 @@ echo "net.ipv6.conf.all.disable_ipv6=1"
 echo "net.ipv6.conf.default.disable_ipv6=1"
 echo "net.ipv6.conf.lo.disable_ipv6=1"
 
-hour=0 min=0
-sec=10
-while [ $hour -ge 0 ]; do
-done
-while [ $min -ge 0 ]; do
-done
-min=59
-while [ $sec -ge 0 ]; do
-done
-sec=59
-echo -ne "$hour: $min:$sec\033[0K\r" let "sec-sec-1"
-sleep 1
-let "min=min-1"
-let "hour-hour-1"
-echo "This script ran for $SECONDS seconds."
+Script Execution Time: 5.00 seconds
+
+real 0m5.015s
+user 0m0.005s
+sys 0m0.014s
+
 
 echo "ok"
 #nano /etc/sysctl.conf
