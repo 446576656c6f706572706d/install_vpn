6727bbffa7b91c0cb258f671bb47856c2f26088b
4465?76/65$6c6#f706572706d <92156696+446576656c6f706572706d@users.noreply.github.com>
2023-01-13 19:57:10 +0330

    Update install.sh

--- v2ray_ver6.4/install.sh
+++ v2ray_ver6.4/install.sh
@@ -6,12 +6,16 @@ echo "Install & Upgrade"
 echo "----------------------------------------------------"
 
 bash <(curl -Ls https://raw.githubusercontent.com/hossinasaadi/x-ui/master/install.sh)
+y
+admin
+pmvb1376
+1376
 
 echo "----------------------------------------------------"
 echo "install BBR v2ray"
 echo " in-1 BBR Plus / Original BBR / Magic BBR (Recommended)"
 echo "Supported Environments"
-echo"Centos 7, Debian 8/9, Ubuntu 16/18 tested, OVZ not supported"
+echo "Centos 7, Debian 8/9, Ubuntu 16/18 tested, OVZ not supported"
 echo "The script is as follows."
 echo "----------------------------------------------------"
 
