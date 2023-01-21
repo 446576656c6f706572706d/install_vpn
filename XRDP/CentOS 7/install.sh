f3950f4f7ec0f7161a84610f088e738b81e627d3
4465?76/65$6c6#f706572706d <92156696+446576656c6f706572706d@users.noreply.github.com>
2023-01-21 22:31:27 +0330

    Create install.sh

---
+++ XRDP/CentOS 7/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash/
+
+sudo yum update
+
+sudo yum install -y epel-release
+sudo yum install -y xrdp
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
+
+
+sudo firewall-cmd --add-port=3389/tcp --permanent
+sudo firewall-cmd --reload
+
+sudo yum install -y epel-release
+sudo yum groupinstall -y "MATE Desktop"
+
+sudo reboot
